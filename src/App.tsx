770bddd0144b3d153dbadc6a241730d7c9b74774
julio mizrahi <julio_mizrahi@hotmail.com>
2023-09-07 01:37:37 +0300

    add-break-space

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import router from "./router/Router";
 import { RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return <RouterProvider router={router} />;
 }
