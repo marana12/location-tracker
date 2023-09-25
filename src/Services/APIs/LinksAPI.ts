20b8ea9029e38a822efe0d238da06c5c74953021
julio mizrahi <julio_mizrahi@hotmail.com>
2023-09-25 14:08:06 +0300

    change-redirect-page-configuration

--- src/Services/APIs/LinksAPI.ts
+++ src/Services/APIs/LinksAPI.ts
@@ -7,15 +7,26 @@ export async function CreateShortUrl(originalUrl: string): Promise<AxiosResponse
   return await HTTP.post<Link>("/api/links/create?originalUrl=" + originalUrl);
 }
 
-export async function GetOriginalUrl(hashedUrl: string | undefined, lat: number | undefined, lon: number | undefined): Promise<Link | undefined> {
+export async function AddVisitor(hashedUrl: string | undefined, lat: number | undefined, lon: number | undefined): Promise<Link | undefined> {
+
+  const params = {
+    hashedUrl: hashedUrl,
+    lat: lat?.toString(),
+    lon: lon?.toString(),
+  }
 
   const cordinate = lat && lon ? `&lat=${lat}&lon=${lon}` : "";
 
-  const response = await HTTP.post<Link>(`/api/links?hashedUrl=${hashedUrl}${cordinate}`);
+  const response = await HTTP.post<Link>(`/api/links/visitor`, params);
 
   return response.data;
 }
 
 export async function GetShortUrlDetails(hashedUrl: string | undefined | null): Promise<AxiosResponse<ShortUrlModel>> {
   return await HTTP.get<ShortUrlModel>(`/api/links/shorturl?hashedUrl=${hashedUrl}`);
+}
+
+export async function GetUrl(hashedUrl: string | undefined): Promise<AxiosResponse<Link>> {
+  return await HTTP.get<Link>(`/api/links/originalurl?hashedUrl=${hashedUrl}`);
+
 }
--- src/components/RedirectPage.tsx
+++ src/components/RedirectPage.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { getLocation } from "../Utils/Navigator";
-import { GetOriginalUrl } from "../Services/APIs/LinksAPI";
+import { AddVisitor, GetUrl } from "../Services/APIs/LinksAPI";
 import LoaderRipple from "./Utils/Loaders/LoaderRipple";
 import { useParams } from "react-router-dom";
 import "../styles/redirectPage.scss";
+import { set } from "ol/transform";
 
 export default function RedirectPage() {
   const params = useParams();
@@ -12,7 +13,21 @@ export default function RedirectPage() {
   const [latitude, setLatitude] = useState<number>();
   const [longitude, setLongitude] = useState<number>();
   const [isBusy, setBusy] = useState<boolean>(true);
-  const [hasLocation, setHasLocation] = useState<boolean>(false);
+  const [originalUrl, setOriginalUrl] = useState<string>();
+
+  useEffect(() => {
+    const getUrl = async () => {
+      const response = await GetUrl(hashUrl);
+      console.log(response);
+      if (response.data) {
+        const { originalUrl } = response.data;
+
+        setOriginalUrl(originalUrl);
+      }
+    };
+
+    getUrl();
+  }, [hashUrl]);
 
   useEffect(() => {
     async function getGeoLocation() {
@@ -21,31 +36,32 @@ export default function RedirectPage() {
 
         if (location) {
           setLatitude(location.latitude);
+
           setLongitude(location.longitude);
         }
       } catch (error) {
         console.error(error);
-      } finally {
-        setHasLocation(true);
       }
     }
 
-    async function getOriginalUrl() {
-      const url = await GetOriginalUrl(hashUrl, latitude, longitude);
+    async function addNewVisitor() {
+      if (originalUrl != undefined) {
+        AddVisitor(hashUrl, latitude, longitude)
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err))
+          .finally(() => {
+            window.location.replace(originalUrl);
 
-      if (url) {
-        window.location.replace(url.originalUrl);
+            setBusy(false);
+          });
       }
-
-      setBusy(false);
     }
 
-    getGeoLocation();
-
-    if (hasLocation && isBusy) {
-      getOriginalUrl();
-    }
-  }, [hasLocation, isBusy]);
+    getGeoLocation().then(() => {
+      console.log("Test");
+      addNewVisitor();
+    });
+  }, [originalUrl != null]);
 
   return (
     <>
